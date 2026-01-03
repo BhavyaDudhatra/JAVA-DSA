1e09fe988932c04d9074e9d5219c53ebe4bf4d04
Bhavya Dudhatra <bhavyadudhatrahere@example.com>
2026-01-04 01:37:36 +0530

    output in java

---
+++ 10. Variables and Data Types/outputInJava.java
@@ -0,0 +1,7 @@
+public class outputInJava {
+    public static void main(String[] args) {
+        System.out.println("String 1");
+        System.out.println(123);
+        System.out.println('d');
+    }
+}
