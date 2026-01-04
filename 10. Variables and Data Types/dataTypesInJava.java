e2e9b26d57c0f19d3fee6c7250f72c5acbf1bc33
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-04 14:47:59 +0530

    data types in java

---
+++ 10. Variables and Data Types/dataTypesInJava.java
@@ -0,0 +1,15 @@
+public class dataTypesInJava {
+    public static void main(String[] args) {
+        int no=123;
+        char ch='a';
+        String str="123qwe";
+        float f=1.2f;
+        boolean b=true;
+
+        System.out.println(no);
+        System.out.println(ch);
+        System.out.println(str);
+        System.out.println(f);
+        System.out.println(b);
+    }
+}
