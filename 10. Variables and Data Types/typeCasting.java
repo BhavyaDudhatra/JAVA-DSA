85597f58d7eb3a2e427a03afc6f50e7cdad4fda4
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-09 00:58:43 +0530

    Type Casting

---
+++ 10. Variables and Data Types/typeCasting.java
@@ -0,0 +1,11 @@
+public class typeCasting{
+    public static void main(String[] args) {
+        int no=3;
+        float flo=12.34f;
+        System.out.println(no);
+        System.out.println(flo);
+        no=(int)(flo);
+        System.out.println(no);
+        System.out.println(flo);
+    }
+}
