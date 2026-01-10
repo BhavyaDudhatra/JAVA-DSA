3ecd93afb1265c20ca8175d021f5108af9fe45d4
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-10 22:43:14 +0530

    type promotion

---
+++ 10. Variables and Data Types/typePromotion.java
@@ -0,0 +1,11 @@
+public class typePromotion{
+    public static void main(String args[]){
+        int a=5;
+        float f=3.3f;
+        float ans=a+f;
+
+        System.out.println("Value of A : " + a);
+        System.out.println("Value of B : " + f);
+        System.out.println("Sum of A and B: " + ans);
+    }
+}
