32a9188cccd42ca5dad44fe3b5f211008a037bba
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-11 23:43:44 +0530

    Operators in java

---
+++ 11. Operators/operators.java
@@ -0,0 +1,10 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=8, b=3;
+        System.out.println("A / B = " + (a/b));
+        System.out.println("A * B = " + (a*b));
+        System.out.println("A + B = " + (a+b));
+        System.out.println("A - B = " + (a-b));
+        System.out.println("A % B = " + (a%b));
+    }
+}
