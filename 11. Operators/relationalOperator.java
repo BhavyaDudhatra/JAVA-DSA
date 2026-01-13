36b642a4eaeaaed8eb9ac7c981554584ca87d350
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-14 00:18:23 +0530

    Relational Operators in JAVA

---
+++ 11. Operators/relationalOperator.java
@@ -0,0 +1,13 @@
+public class relationalOperator{
+    public static void main(String[] args) {
+        System.out.println("Equal to : '='");
+        System.out.println("Not Equal to : '!='");
+        System.out.println("Equal to Equal to : '=='");
+        System.out.println("Equal to-if data type is same : '==='");
+        System.out.println("Less than : '<'");
+        System.out.println("Less than Equal to : '<='");
+        System.out.println("Greater Than : '>'");
+        System.out.println("Greater Than Equal to : '>='");
+        
+    }
+}
