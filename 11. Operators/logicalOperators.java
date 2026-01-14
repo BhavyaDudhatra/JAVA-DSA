2bade74b723426f6516ff2b38c08d879b3f53aa2
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-14 22:08:57 +0530

    Logical Operators in JAVA

---
+++ 11. Operators/logicalOperators.java
@@ -0,0 +1,7 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+        System.out.println("Logical AND : '&&'");
+        System.out.println("Logical OR : '||'");
+        System.out.println("Logical NOT : '!'");
+    }
+}
