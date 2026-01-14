1e055753c742c15a407107b1c2fae8318bbd4d44
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-15 00:14:52 +0530

    Assignment Operators in JAVA

---
+++ 11. Operators/assignmentOperators.java
@@ -0,0 +1,33 @@
+public class assignmentOperators {
+    public static void main(String[] args) {
+        int a=5,b=3;
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+        System.out.println();
+        a+=b;
+        System.out.println("a+=b");
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+        System.out.println();
+        a-=b;
+        System.out.println("a-=b");
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+        System.out.println();
+        a*=b;
+        System.out.println("a*=b");
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+        System.out.println();
+        a/=b;
+        System.out.println("a/=b");
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+        System.out.println();
+        a=b;
+        System.out.println("a=b");
+        System.out.println("A : "+a);
+        System.out.println("B : "+b);
+
+    }
+}
