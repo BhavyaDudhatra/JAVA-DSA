d38732638686ae915419aeba439b315b667a3473
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-16 23:02:00 +0530

    If Else statement in JAVA

---
+++ 12. Conditional Statements/ifElse.java
@@ -0,0 +1,10 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int age=2;
+        if(age>=18){
+            System.out.println("Can vote.");
+        }else{
+            System.out.println("Can't vote.");
+        }
+    }
+}
