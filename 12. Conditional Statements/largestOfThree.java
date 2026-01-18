13fe7d4f4b36a795438269253e851b8cb79ab920
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-18 12:35:26 +0530

    Print the greatest of two numbers using if...else statements

---
+++ 12. Conditional Statements/largestOfThree.java
@@ -0,0 +1,12 @@
+public class largestOfThree {
+    public static void main(String[] args) {
+        int a=17,b=17;
+        if(a>b){
+            System.out.println(a+" is greater than "+b);
+        }else if(b>a){
+            System.out.println(b+" is greater than "+a);
+        }else{
+            System.out.println("Both are same.");
+        }
+    }
+}
