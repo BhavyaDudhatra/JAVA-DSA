d6c8f067491a31ab259d521d6d596ac6f78c3f6f
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-20 00:04:10 +0530

    Ternary Operator in Java

---
+++ 12. Conditional Statements/ternaryOperator.java
@@ -0,0 +1,9 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int a=31,b=55;
+
+        String ans=a>b?"'A' is greater":"'B' is greater";
+        System.out.println(ans);
+        // System.out.println(a>b?"'A' is greater":"'B' is greater");
+    }
+}
