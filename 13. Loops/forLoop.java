291e007da1906369a16c5c212e13e7521a100152
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-01-27 22:11:19 +0530

    For Loop in JAVA

---
+++ 13. Loops/forLoop.java
@@ -0,0 +1,9 @@
+public class forLoop{
+    public static void main(String args[]){
+        System.out.println("Print 'Hello' 5 times");
+        System.out.println();
+        for(int i=0;i<5;i++){
+            System.out.println((i+1)+". Hello");
+        }
+    }
+}
