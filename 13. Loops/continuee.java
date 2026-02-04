052744738db11ec30ca31bfc762d051702b029cf
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-02-04 22:24:52 +0530

    Continue key word in java

---
+++ 13. Loops/continuee.java
@@ -0,0 +1,11 @@
+public class continuee {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            if(i==5){
+                continue;
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
