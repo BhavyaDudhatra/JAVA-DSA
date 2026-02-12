b6fce496c5e589f6c3d85ba7003414cca7d13f8c
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-02-12 17:27:32 +0530

    Syntex of Function and code demo in java

---
+++ 15. Function and Methods/syntexOfFunctionCodeDemo.java
@@ -0,0 +1,12 @@
+public class syntexOfFunctionCodeDemo {
+    public static void print1(){
+        System.out.println("Hello1");
+    }
+    static void print2(){
+        System.out.println("Hello2");
+    }
+    public static void main(String args[]){
+        print1();
+        print2();
+    }
+}
