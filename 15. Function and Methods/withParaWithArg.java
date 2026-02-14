829f195f0414e6042beba09458f202dab6152be1
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-02-14 18:52:43 +0530

    example of With argue and With return in java

---
+++ 15. Function and Methods/withParaWithArg.java
@@ -0,0 +1,9 @@
+public class withParaWithArg {
+    public static void main(String[] args) {
+        int a=3,b=6;
+        System.out.println("Sum of A and B : "+sum(a,b));
+    }
+    static int sum(int a,int b){
+        return a+b;
+    }
+}
