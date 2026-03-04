d270ee1e0a452a1aaa541d78a146e102d962c3c1
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-03-04 14:31:49 +0530

    Solid Rhombus Pattern in java

---
+++ 16. Pattern (Part II) - Advanced/solidRhombusPattern.java
@@ -0,0 +1,14 @@
+class solidRhombusPattern{
+    public static void main(String[] args) {
+        int no=5;
+        for(int i=0;i<no;i++){
+            for(int j=i;j<no;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<no;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
