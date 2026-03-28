8ddd6870edd3318000dd8d04f00f1b7006010b5c
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-03-28 23:31:55 +0530

    Buy Sell Stock program in java

---
+++ 18. Array (Part II)/BuySellStock.java
@@ -0,0 +1,28 @@
+public class BuySellStock {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            // Update minimum price so far
+            if (price < minPrice) {
+                minPrice = price;
+            } 
+            // Calculate profit if sold today
+            else {
+                int profit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum Profit: " + maxProfit(prices));
+    }
+}
