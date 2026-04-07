5358724f0af80018d2025ddecb56d9106475cde6
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-04-07 18:42:17 +0530

    Diagonal Sum in matrix in java

---
+++ 20. 2D Array/DiagonalSum.java
@@ -0,0 +1,23 @@
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int n = matrix.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += matrix[i][i];
+
+            if (i != n - i - 1) {
+                sum += matrix[i][n - i - 1];
+            }
+        }
+
+        System.out.println("Diagonal Sum = " + sum);
+    }
+}
