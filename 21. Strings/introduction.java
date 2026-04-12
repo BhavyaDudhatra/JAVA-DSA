047e0cc97cce7dbc314b2064bb3448c66c83e9da
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-04-12 16:23:52 +0530

    Introduction to Strings

---
+++ 21. Strings/introduction.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class introduction {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name=sc.next();
+        System.out.println("Your name : "+name);
+        sc.close();
+    }
+}
