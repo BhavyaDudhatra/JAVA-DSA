32f58fef2ad45faa747c469d09b6b3239c814fb7
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-04-28 22:47:27 +0530

    String Compression in java

---
+++ 21. Strings/StringCompression.java
@@ -0,0 +1,36 @@
+public class StringCompression {
+
+    public static String compressString(String str) {
+        // Edge case
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+
+        StringBuilder compressed = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
+                compressed.append(str.charAt(i - 1));
+                compressed.append(count);
+                count = 1;
+            }
+        }
+
+        // Append last character and its count
+        compressed.append(str.charAt(str.length() - 1));
+        compressed.append(count);
+
+        return compressed.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "aaabbc";
+        String result = compressString(input);
+
+        System.out.println("Original String: " + input);
+        System.out.println("Compressed String: " + result);
+    }
+}
