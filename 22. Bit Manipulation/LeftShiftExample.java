3c6c54f0007c5bc4badd0cda3485efa7a2b6f27b
Bhavya Dudhatra <bhavyadudhatrahere@gmail.com>
2026-05-06 16:34:10 +0530

    binary left shift example in java

---
+++ 22. Bit Manipulation/LeftShiftExample.java
@@ -0,0 +1,9 @@
+public class LeftShiftExample {
+    public static void main(String[] args) {
+        int number = 5;       // Binary: 0000 0101
+        int result = number << 2;
+
+        System.out.println("Original number: " + number);
+        System.out.println("After left shift by 2: " + result);
+    }
+}
